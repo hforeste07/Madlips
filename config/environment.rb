cfefdf90e99ba9a2feb45a1a6b5b6daa8c1ed9bc
Heather Forester <hforeste07@gmail.com>
2025-05-12 17:30:26 -0500

    Initialize fix-fly-destroyed-db

---
+++ config/environment.rb
@@ -0,0 +1,37 @@
+require './app'
+
+configure do
+  # To open .html.erb files, need to register them
+  Tilt.register(Tilt::ERBTemplate, 'html.erb')
+  
+  # alternative to setting the layout in every erb() call
+  # we can set the default layout name to use and sinatra 
+  # will look for that file
+  set(:erb, :layout => :application_layout)
+
+  # setup a database connection
+  set(:database, {adapter: "sqlite3", database: ENV.fetch('DATABASE_LOCATION', 'db/development.sqlite3')})
+end
+
+configure :development do
+  require "sinatra/reloader"
+
+  # we would also like a nicer error page in development
+  require 'better_errors'
+  require 'binding_of_caller'
+  
+  # need this configure for better errors
+  use(BetterErrors::Middleware)
+  BetterErrors.application_root = __dir__
+  BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+  # appdev support patches
+  require "appdev_support"
+
+  AppdevSupport.config do |config|
+    # config.action_dispatch = true;
+    # config.active_record = true;
+    config.pryrc = :full;
+    end
+  AppdevSupport.init
+end
